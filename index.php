e84df375da7294f79290807e6bb7ad359dc31354
aftab70 <43289912+aftab70@users.noreply.github.com>
2023-05-08 17:50:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -49,13 +49,13 @@ echo "You are connected with " .$localIP;
   <img src="	https://media-exp1.licdn.com/dms/image/C4D03AQG5fx…eta&t=hEn0QnbowlUQJKjQ9MfYvh5r2gMqIVd9__gehY0eeBE" alt="" style="width:100%">
   <h1>Aftab Ali</h1>
   <p> Cloud and DevOps consultant </p>
-  <p> Deployed uisng Azure v1 </p>
+  <p> Deployed uisng Azure v2 </p>
   <div style="margin: 24px 0;"> 
     <a href="https://www.facebook.com/aftab.shaikh.589"><i class="fa fa-facebook"></i></a>  
     <a href="https://www.linkedin.com/in/aftab-shaikh-035440118/"><i class="fa fa-linkedin"></i></a>  
     <a href="https://twitter.com/aftab70_ali"><i class="fa fa-twitter"></i></a> 
   </div>
-  <p><button>Email Id - aftab70@ymail.com</button></p>
+  <p><button>Email Id - aftabali025@gmail.com</button></p>
 </div>
 
 </body>
